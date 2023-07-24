16618c6e6778b706d4c5cb57aa88c7f4795f3071
saivardhan8729 <119431804+saivardhan8729@users.noreply.github.com>
2023-07-24 22:08:43 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,26 @@
+let displayValue = '';
+
+function appendToDisplay(value) {
+    displayValue += value;
+    updateDisplay();
+}
+
+function clearDisplay() {
+    displayValue = '';
+    updateDisplay();
+}
+
+function calculateResult() {
+    try {
+        const result = eval(displayValue);
+        displayValue = result.toString();
+        updateDisplay();
+    } catch (error) {
+        displayValue = 'Error';
+        updateDisplay();
+    }
+}
+
+function updateDisplay() {
+    document.getElementById('display').value = displayValue;
+}
